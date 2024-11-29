a0c63f622c8ffb978d882ee373c423d5a82294a7
ShankarS12 <135296879+ShankarS12@users.noreply.github.com>
2024-11-29 13:18:46 +0530

    eclipseday6part1

---
+++ ForLoop.java
@@ -0,0 +1,30 @@
+package day6;
+
+import java.util.*;
+
+public class ForLoop {
+	public static void main(String[]args) {
+		
+//		for (int i = 0; i <=200; i++) {
+//			if(i == 50) {
+//				continue;
+//			}
+//			else if(i == 101) {
+//				break;
+//			}
+//			else {
+//				System.out.println(i);
+//			}
+//			
+//		}
+		
+		//String Concept
+		
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		
+		System.out.println(name.toUpperCase());
+		System.out.println(name.toLowerCase());
+;	}
+
+}
