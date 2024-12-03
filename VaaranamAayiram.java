88f475f42c468586a1619c804fd71e657fb026a5
ShankarS12 <135296879+ShankarS12@users.noreply.github.com>
2024-12-03 12:57:00 +0530

    eclipseday8

---
+++ VaaranamAayiram.java
@@ -0,0 +1,18 @@
+package day8;
+
+public class VaaranamAayiram {
+	
+	public static void main(String[]args) {
+		
+		String name = "dead";
+		String str = new String("dead");
+		
+		if (name.equals(str)) {
+			System.out.println("Surya weds Mehana");
+		}
+		else {
+			System.out.println("Surya weds Ramya");
+		}
+	}
+
+}
